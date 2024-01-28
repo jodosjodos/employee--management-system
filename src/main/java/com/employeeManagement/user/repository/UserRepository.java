1c218069b50f7d155dee7d1a434a9b659a1d3be1
jodosjodos <jeandedieu2030@gmail.com>
2024-01-28 11:43:32 +0200

    adding  comments

--- src/main/java/com/employeeManagement/user/repository/UserRepository.java
+++ src/main/java/com/employeeManagement/user/repository/UserRepository.java
@@ -7,7 +7,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-
+// user repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
    @Transactional
